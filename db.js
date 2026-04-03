32b347b938b738588c786f58566ae9f3aad7f86f
Daniel Tenório <daniels2l@hotmail.com>
2026-04-03 00:43:23 -0300

    Update DB.js
    Atualizei pq errei aq vei

--- db.js
+++ db.js
@@ -1,18 +1,11 @@
-const mysql = require('mysql2');
+const Sequelize = require("sequelize")
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'Doacao@localhost',          // seu usuário do MySQL Workbench
-  password: '757598', // sua senha do MySQL Workbench
-  database: 'doafacil'   // nome do banco que você vai criar
-});
+const sequelize = new Sequelize('doacoes', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
-    return;
-  }
-  console.log('Conectado ao banco de dados MySQL!');
-});
-
-module.exports = connection;
+module.exports = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+}
